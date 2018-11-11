16e732c33e46543bf2fe795228fb1f9417eb3952
haotrg035 <haotrg035@gmail.com>
2018-11-12 02:29:41 +0700

    Gan xong roi~

--- ketnoicsdl/thuvien.php
+++ ketnoicsdl/thuvien.php
@@ -1,22 +1,71 @@
 <?php
-    function travemang($sql,$conn){
-		$rs = $conn->query($sql);
-		$rows = [];
-		while ($row = mysqli_fetch_array($rs)) {
-			$rows[] = $row;
-		}
-		return $rows;
+/**
+ * thu vien php
+ *
+ * @author haotrg035
+ */
+include_once 'ketnoi.php';
+    function travemang($sql){
+        global $conn;
+        $rs = $conn->query($sql);
+        $rows = [];
+        if($rs) {
+		    while ($row = mysqli_fetch_array($rs)) {
+			    $rows[] = $row;
+            }
+            return $rows;
+        }else echo "Query Fail: ".mysqli_error($conn);
+    }
+    //lấy 4 đáp án của một câu hỏi
+    function laydapantheomacauhoi($macauhoi){
+        global $conn;
+        $sql = "SELECT * FROM `dapan` WHERE `dapan`.`macauhoi`='$macauhoi'";
+        return travemang($sql);
+    }
+    //dùng vòng lặp để lấy hết đáp án của một mảng câu hỏi
+    function laylistdapan($mangcauhoi){
+        $rs = [];
+        foreach ($mangcauhoi as $keycauhoi => $rowcauhoi) {
+            $macauhoi = $rowcauhoi['macauhoi'];
+            $dapan=laydapantheomacauhoi($macauhoi);
+            
+            $rs["$macauhoi"] = array();
+
+            foreach ($dapan as $keydapan => $rowdapan) {
+                array_push($rs["$macauhoi"],$rowdapan['noidung']);
+            }
+        }
+        return $rs;
+    }
+    function laycauhoitravemang($mamon,$socaude,$socautb,$socaukho){
+        global $conn;
+        $rs = [];
+        if ($socaude != 0) {
+            $sql = "SELECT `tvcauhoi`.`intdokho` AS `dokho`,`tvcauhoi`.`macauhoi`,`tvcauhoi`.`noidung` AS `noidungcauhoi`
+            FROM `tvcauhoi` WHERE `tvcauhoi`.mamon = '$mamon' AND `tvcauhoi`.`intdokho` = 1
+            ORDER BY RAND() LIMIT $socaude";
+            $rs=array_merge($rs,travemang($sql));
+        }
+        if ($socaude != 0) {
+            $sql = "SELECT `tvcauhoi`.`intdokho` AS `dokho`,`tvcauhoi`.`macauhoi`,`tvcauhoi`.`noidung` AS `noidungcauhoi`
+            FROM `tvcauhoi` WHERE `tvcauhoi`.mamon = '$mamon' AND `tvcauhoi`.`intdokho` = 2
+            ORDER BY RAND() LIMIT $socaude";
+            $rs=array_merge($rs,travemang($sql));
+        }
+        if ($socaude != 0) {
+            $sql = "SELECT `tvcauhoi`.`intdokho` AS `dokho`,`tvcauhoi`.`macauhoi`,`tvcauhoi`.`noidung` AS `noidungcauhoi`
+            FROM `tvcauhoi` WHERE `tvcauhoi`.mamon = '$mamon' AND `tvcauhoi`.`intdokho` = 3
+            ORDER BY RAND() LIMIT $socaude";
+            $rs=array_merge($rs,travemang($sql));
+        }
+       return $rs;
     }
 ?>
-<?php 
-    
-?>
-
 <?php
-    function hienthicauhoi($mang){
-        shuffle($mang);
-        $j = 1;
-        foreach ($mang as $key => $value) { ?>
+//hiển thị câu hỏi + đáp án theo câu hỏi ra dạng bảng
+    function hienthicauhoi($cauhoi,$dapan){
+        $j = 1;;
+        foreach ($cauhoi as $key => $value) { ?>
             <table border="1" style="border-collapsse: collapse;">
                 <tr>
                     <td width = "300" colspan="2">
@@ -25,10 +74,10 @@
                 </tr>
                 <tr>
                     <td colspan="2">
-                        <ol type="A" style="Width: 400px; postion:relative">
+                        <ol type="A" style="Width: 800px; postion:relative">
                             <?php 
                                 for ($i=0; $i < 4; $i++) { ?>
-                                    <li style="float:left;width:40%;"><?php echo $i ?></li>
+                                    <li style="float:left;width:45%;"><?php echo $dapan[$value['macauhoi']][$i]; ?></li>
                                 <?php }?>
                         </ol>
                     </td>
@@ -36,5 +85,11 @@
             </table>
         <?php
     $j++;   }
+    }
+?>
+<?php
+    function traodoicauhoi($mang){
+        shuffle($mang);
+        hienthicauhoi($mang);
     }
 ?>
--- taodethi/view/test.php
+++ taodethi/view/test.php
@@ -1,18 +1,22 @@
 <?php
-include "../../ketnoicsdl/ketnoi.php";
 include "../../ketnoicsdl/thuvien.php";
 
 $mamon = "HH";
 $madethi = "Exam01";
-$socaude = 5;
-$socautb = 5;
-$socaukho = 5;
+$socaude = 1;
+$socautb = 1;
+$socaukho = 1;
 
-function laycauhoitudong($mamon,$socaude,$socautb,$socaukho){
-    if ($mamon != 0) {
-        $rscaude = [];
-        $sql_caude = "SELECT * FROM `tvcauhoi` WHERE `tvcauhoi`.mamon = $mamon 
-        ORDER BY RAND() LIMIT $socaude";
-    }
 
-}
+$cauhoi = laycauhoitravemang($mamon,$socaude,$socautb,$socaukho);
+$dapan = laylistdapan($cauhoi);
+// print_r($dapan);
+// foreach ($cauhoi as $key => $value) {
+//     echo print_r($value) ."<br>";
+// }
+// echo "<hr>";
+// foreach ($dapan as $key => $value) {
+//     echo print_r($value) ."<br>";
+// }
+// echo print_r($dapan);
+hienthicauhoi($cauhoi,$dapan);
